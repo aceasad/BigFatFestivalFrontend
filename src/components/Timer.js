2e38495eaaee16e0379011623be428474aa50daa
Asad Saeed <asad.saeed@live.com>
2022-09-01 00:37:12 +0500

    Updated Font

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -2,7 +2,6 @@ import react, { useState, useEffect } from 'react'
 
 const Timer = () => {
 
-    const [days, setDays] = useState(0);
     const [hours, setHours] = useState(0);
     const [minutes, setMinutes] = useState(0);
     const [seconds, setSeconds] = useState(0);
@@ -10,7 +9,6 @@ const Timer = () => {
 
     const getTime = () => {
         const time = Date.parse(deadline) - Date.now();
-        setDays(Math.floor(time / (1000 * 60 * 60 * 24)));
         setHours(Math.floor((time / (1000 * 60 * 60)) % 24));
         setMinutes(Math.floor((time / 1000 / 60) % 60));
         setSeconds(Math.floor((time / 1000) % 60));
@@ -23,9 +21,8 @@ const Timer = () => {
     return (
         <div className="timer" role="timer">
             <span className="full-timer ">
-                {days < 10 ? "0" + days : days} :
-                {hours < 10 ? "0" + hours : hours} :
-                {minutes < 10 ? "0" + minutes : minutes} :
+                {hours < 10 ? "0" + hours : hours}:
+                {minutes < 10 ? "0" + minutes : minutes}:
                 {seconds < 10 ? "0" + seconds : seconds}
             </span>
         </div>)
